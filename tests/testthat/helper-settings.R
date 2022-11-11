7ceef71e0912238e679bffb5e186e3a3f0e7afcd
Mihai Valcu <valcu@orn.mpg.de>
2022-11-11 16:25:50 +0100

    .my.cnf

--- tests/testthat/helper-settings.R
+++ tests/testthat/helper-settings.R
@@ -2,6 +2,11 @@
 if (Sys.getenv("GITHUB_ACTIONS") == "true") {
 
 # see https://github.com/marketplace/actions/actions-setup-mysql
-options(dbo.my.cnf = "/etc/my.cnf")
+
+file.copy(system.file("my.cnf", package = "dbo"), "/home/runner/.my.cnf")
+
+
+
+options(dbo.my.cnf = "/home/runner/.my.cnf")
 
 }
