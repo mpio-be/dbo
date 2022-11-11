41b1c10a148417078b149ed9d9e668027b2c4416
Mihai Valcu <valcu@orn.mpg.de>
2022-11-11 17:27:15 +0100

    try once more with my.clf

--- tests/testthat/helper-settings.R
+++ tests/testthat/helper-settings.R
@@ -3,10 +3,11 @@ if (Sys.getenv("GITHUB_ACTIONS") == "true") {
 
 # see https://github.com/marketplace/actions/actions-setup-mysql
 
-file.copy(system.file("my.cnf", package = "dbo"), "/home/runner/.my.cnf")
+file.copy(system.file("my.cnf", package = "dbo"), "~/.my.cnf")
 
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print(getwd())
 
-
-options(dbo.my.cnf = "/home/runner/.my.cnf")
+options(dbo.my.cnf = "~/.my.cnf")
 
 }
