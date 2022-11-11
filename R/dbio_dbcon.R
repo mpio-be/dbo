e2bc4a871fdd8f8437785841ae4a39e7314e6a5e
Mihai Valcu <valcu@orn.mpg.de>
2022-11-11 15:47:28 +0100

    using mysql@v1.14.0

--- R/dbio_dbcon.R
+++ R/dbio_dbcon.R
@@ -5,7 +5,7 @@
 #' @description     \code{dbcon} returns a database connection.
 #'                   If user and password are not given dbq looks for TODO
 #' 
-#' @param server       server name. 
+#' @param server       server name. IF more server names are given they will be try out in the given order.
 #' @param db           active database name
 #' @param config.file  path to a config file. See [dbo::my.cnf()]
 #' @param driver       MariaDB, defaults to MariaDB
@@ -24,9 +24,22 @@ dbcon <- function(server = c("scidb","scidb_replica"), db , config.file = getOpt
       drv          = RMariaDB::MariaDB(),
       # timezone     = "Europe/Berlin", TODO
       default.file = config.file, 
-      group        = server,
+      group        = server[1],
+      timeout      = 3600
+    ) |> try(silent = TRUE)
+
+    if(length(server)> 1 && inherits(con, "try-error")){
+      warning(glue("Connection to {dQuote(server[1])} failed, using { dQuote(server[2]) }."))
+
+    con <- dbConnect(
+      drv          = RMariaDB::MariaDB(),
+      default.file = config.file,
+      group        = server[2],
       timeout      = 3600
     )
+
+
+    }
 
     if (!missing(db)) {
       dbExecute(con, glue("USE {db}"))
